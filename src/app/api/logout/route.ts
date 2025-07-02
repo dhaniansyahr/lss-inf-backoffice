df6e9c8d43da1a6d921e46a3c1b785e34167a0e7
Rigen Maulana <rygen.dev@gmail.com>
2025-07-03 01:48:14 +0700

    fix: change logout redirect

--- src/app/api/logout/route.ts
+++ src/app/api/logout/route.ts
@@ -6,10 +6,12 @@ import { cookies } from "next/headers";
 
 import { sessionCookieKey } from "@/constants/session";
 
-export async function POST() {
+export async function GET() {
   const cookieStore = await cookies();
 
   cookieStore.delete(sessionCookieKey);
 
-  return NextResponse.json({ message: "Session deleted" });
+  return NextResponse.redirect(
+    new URL("/login", process.env.NEXT_PUBLIC_FE_URL),
+  );
 }
